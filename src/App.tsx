79c419675e33b71791759e1104664dccce88a174
Mattee37 <bluestacksmm.137@gmail.com>
2021-08-21 02:11:29 -0300

    Third Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
 
 import ComponenteHijo from "./components/ComponenteHijo";
 import Encabezado from "./components/Encabezado";
 
-interface InitialComponents {
+interface Components {
   texto: string;
   adderValue: number;
   key: number;
@@ -13,22 +13,29 @@ interface InitialComponents {
 const App: FC = () => {
   console.log("App renderizada");
 
-  const initialComponents: Array<InitialComponents> = [
+  const [components, setComponents] = useState<Array<Components>>([
     { texto: "Contador 1", adderValue: 1, key: 1 },
     { texto: "Contador 2", adderValue: 2, key: 2 },
     { texto: "Contador 3", adderValue: 3, key: 3 },
-  ];
+  ]);
 
   const addComponent = () => {
-    console.log("agregado");
-    initialComponents.push({ texto: "TEST", adderValue: 1, key: 100 });
+    const randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    setComponents(
+      components.concat({
+        texto: "Contador " + randomNumber,
+        adderValue: randomNumber,
+        key: randomNumber + randomNumber,
+      })
+    );
+    console.log(components);
   };
 
   return (
     <div className="grid">
-      <button onClick={addComponent}>Agregar componente!</button>
       <Encabezado />
-      {initialComponents.map(({ texto, adderValue, key }) => (
+      <button onClick={addComponent}>Agregar componente!</button>
+      {components.map(({ texto, adderValue, key }) => (
         <ComponenteHijo texto={texto} adderValue={adderValue} key={key} />
       ))}
     </div>
